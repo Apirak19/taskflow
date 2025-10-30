78bfba8ea14ccedb449596a1853039ef92dfa538
Apirakl9 <apirakfakin@gmail.com>
2025-10-30 21:47:21 +0700

    fix: remove PGPORT

--- src/config/db.js
+++ src/config/db.js
@@ -14,7 +14,7 @@ const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  // port: process.env.PGPORT,
 });
 
 pool.on("connect", () => {
